b1bfed11b477f521e8dde3d6980c7ce5563b4451
anbuz <anbuz@yandex.ru>
2021-12-25 18:05:20 +0300

    hw16-io.  Second commit

--- hw16-io/src/main/java/ru/buz/dataprocessor/FileSerializer.java
+++ hw16-io/src/main/java/ru/buz/dataprocessor/FileSerializer.java
@@ -1,7 +1,10 @@
 package ru.buz.dataprocessor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -17,12 +20,13 @@ public class FileSerializer implements Serializer {
     @Override
     public void serialize(Map<String, Double> data) {
         ObjectMapper objectMapper = new ObjectMapper();
-        try {
+        try{
             String json = objectMapper.writeValueAsString(data);
             json = json.replace("\\\"","");
+//            objectMapper.writeValue(new File(fileName),json);
             Files.writeString(Paths.get(fileName), json);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new UncheckedIOException(e);
         }
         //формирует результирующий json и сохраняет его в файл
     }
