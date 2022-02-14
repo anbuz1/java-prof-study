902f6d6133dfa841174ebdce13c1d11ad48d1359
anbuz <anbuz@yandex.ru>
2022-02-14 23:44:55 +0300

    hw31-executors. first commit

---
+++ hw31-executors/src/main/java/MultiThread.java
@@ -0,0 +1,54 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MultiThread {
+    private final int COUNT_LIMIT = 10;
+    private static final Logger logger = LoggerFactory.getLogger(MultiThread.class);
+    private boolean whoIsInCharge = true;
+    private boolean downFlag;
+    private int count = 1;
+
+    private synchronized void counting() {
+        try {
+            while (count != 0) {
+                this.notifyAll();
+                logger.info(String.valueOf(count));
+
+                if (whoIsInCharge) {
+                    whoIsInCharge = false;
+
+                    while (!whoIsInCharge) {
+                        this.wait();
+                    }
+                } else {
+                    whoIsInCharge = true;
+                    doUpAndDown();
+                    if (count != 0) {
+                        while (whoIsInCharge) {
+                            this.wait();
+                        }
+                    }
+                }
+            }
+        } catch (InterruptedException ex) {
+            logger.info(ex.toString());
+        }
+    }
+
+    private void doUpAndDown() {
+        if (count == COUNT_LIMIT) {
+            downFlag = true;
+        }
+        if (downFlag) {
+            count--;
+        } else {
+            count++;
+        }
+    }
+
+    public static void main(String[] args) {
+        MultiThread multiThread = new MultiThread();
+        new Thread(multiThread::counting).start();
+        new Thread(multiThread::counting).start();
+    }
+}
